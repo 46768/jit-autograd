fde47afb1c4d55cfab4a0cc180e7a9ff3c52b810
46768 <46768@bodin.ac.th>
2025-03-30 08:41:30 -0700

    feat: add logger

--- lib/util/header/allocator.h
+++ lib/util/header/allocator.h
@@ -11,6 +11,25 @@ static inline void* allocate(size_t ptr_size) {
 	return ptr;
 }
 
+static inline void* callocate(size_t size, size_t t_size) {
+	void* ptr = calloc(size, t_size);
+	if (ptr == NULL) {
+		abort();
+	}
+	return ptr;
+}
+
+static inline void* reallocate(void* ptr, size_t size) {
+	if (ptr == NULL) {
+		abort();
+	}
+	void* ptr_new = reallocate(ptr, size);
+	if (ptr_new == NULL) {
+		abort();
+	}
+	return ptr_new;
+}
+
 static inline void deallocate(void* ptr) {
 	if (ptr != NULL) {
 		free(ptr);
---
+++ lib/util/header/logger.h
@@ -0,0 +1,50 @@
+// Uncomment to enable debugging
+//#define DEBUG_MODE
+// Uncomment to enable verbose logging
+//#define VERBOSE_MODE
+
+#ifndef COM_LOGGER_H
+#define COM_LOGGER_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+// Level - type
+// 0 - INFO
+// 1 - WARN
+// 2 - ERROR
+// 3 - DEBUG
+// 4 - FATAL
+
+void _log(int, const char*, int, const char*, const char*, ...);
+
+#define info(format, ...) _log(0, __FILE__, __LINE__, __func__, format, ##__VA_ARGS__)
+#define warn(format, ...) _log(1, __FILE__, __LINE__, __func__, format, ##__VA_ARGS__)
+#define error(format, ...) _log(2, __FILE__, __LINE__, __func__, format, ##__VA_ARGS__)
+#define fatal(format, ...) _log(4, __FILE__, __LINE__, __func__, format, ##__VA_ARGS__);abort()
+#define printr(format, ...) fprintf(stderr, format, ##__VA_ARGS__)
+#define newline() fprintf(stderr, "\n")
+
+#ifdef DEBUG_MODE
+#	define debug(format, ...) _log(3, __FILE__, __LINE__, __func__, format, ##__VA_ARGS__)
+#	define printr_d(format, ...) fprintf(stderr, format, ##__VA_ARGS__)
+#	define newline_d() newline()
+#else
+#	define debug(format, ...)
+#	define printr_d(format, ...)
+#	define newline_d()
+#endif
+
+#ifdef VERBOSE_MODE
+#	define info_v(format, ...) log(format, ##__VA_ARGS__)
+#	define warn_v(format, ...) warn(format, ##__VA_ARGS__)
+#	define error_v(format, ...) error(format, ##__VA_ARGS__)
+#	define debug_v(format, ...) debug(format, ##__VA_ARGS__)
+#else
+#	define info_v(format, ...)
+#	define warn_v(format, ...)
+#	define error_v(format, ...)
+#	define debug_v(format, ...)
+#endif
+
+#endif
